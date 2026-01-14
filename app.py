a38916d590bd91ee68504f3a66a8b7701361e102
SwayamGoyal11 <swayamgoyal4u@gmail.com>
2026-01-14 20:59:29 +0530

    Modify app.py for new page title and layout
    Updated app.py to set page title for Interview Docket Generator and maintain layout.

--- app.py
+++ app.py
@@ -1,23 +1,3 @@
-''' 
-app.py: 
-
-Must only include:
-- File upload widgets
-- Button clicks
-- Calling relevant pipeline functions
-- Displaying output
-
-app.py must not be very detailed, this file only calls functions and renders streamlit. 
-All other functions to go into respective files
-
-- example code:
-import streamlit as st
-from ui.layout import render_app
-
-st.set_page_config(page_title="Demo App")
-
-render_app()
-'''
 import streamlit as st
 from ui.layout import render_app
 
