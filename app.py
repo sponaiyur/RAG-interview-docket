edcf438a668287317b0c9e05db27e2182de43954
SwayamGoyal11 <swayamgoyal4u@gmail.com>
2026-01-14 21:11:39 +0530

    Initialize state before rendering the app

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 from ui.layout import render_app
+from ui.state import init_state
 
 st.set_page_config(page_title="Interview Docket Generator", layout="wide")
 
+init_state()
 render_app()
