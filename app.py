415037596d2c28910f14f52e2525419cfd534e50
SwayamGoyal11 <swayamgoyal4u@gmail.com>
2026-01-14 20:58:03 +0530

    Update Streamlit app title and layout configuration

--- app.py
+++ app.py
@@ -18,3 +18,9 @@ st.set_page_config(page_title="Demo App")
 
 render_app()
 '''
+import streamlit as st
+from ui.layout import render_app
+
+st.set_page_config(page_title="Interview Docket Generator", layout="wide")
+
+render_app()
